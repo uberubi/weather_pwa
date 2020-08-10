3e527f9da1fa04aba541f9d0ea541406bd554c06
Andrew A. Stenipanin <and5251@yandex.ru>
2020-08-10 22:28:23 +0300

    finish ui and bll

--- src/App.js
+++ src/App.js
@@ -1,10 +1,51 @@
-import React from 'react';
-import { fetchWeather } from './api/fetchWeather';
-import  './App.css'
-const App = () => {
-  return (
-    <h1>App</h1>
-  )
-}
+import React, { useState } from "react";
+import { fetchWeather } from "./api/fetchWeather";
+import "./App.css";
+import { convertor } from "./utils/convertor";
 
-export default App
+const App = () => {
+
+  const [query, setQuery] = useState('')
+  const [weather, setWeather] = useState({})
+console.log(convertor(292))
+  const search = async (e) => {
+    if (e.key === 'Enter') {
+      const data = await fetchWeather(query)
+      console.log(data)
+      setWeather(data)
+      setQuery('')
+    }
+  }
+
+  return (
+    <div className="main-container">
+      <input
+        type="text"
+        className="search"
+        placeholder="Search..."
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        onKeyPress={search}
+      />
+
+      {weather.main && (
+        <div className="city">
+          <h2 className="city-name">
+            <span>{weather.name}</span>
+            <span>{weather.sys.country}</span>
+          </h2>
+          <div className="city-temp">
+            {Math.round(weather.main.temp)}
+            <sup>&deg;C</sup>
+          </div>
+          <div className="info">
+            <img className="city-icon" src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt="city icon"/>
+            <p>{weather.weather[0].description}</p>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default App;
---
+++ src/utils/convertor.js
@@ -0,0 +1,1 @@
+export const convertor = (fahrenheit) => (fahrenheit - 32) * 5 / 9
